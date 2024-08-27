4cd6424801af2089c04122bc6e05096253175243
Balu-Nagireddy <133484305+Balu-Nagireddy@users.noreply.github.com>
2024-08-27 14:40:48 +0530

    Delete ForLoop.java

--- ForLoop.java
+++
@@ -1,9 +0,0 @@
-package assignment;
-public class ForLoop {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
-    }
-}
-
